ba951202a918a74e959b4c17d1f4b823de38a16c
Gubio Garcia <gubiogarcia@gmail.com>
2023-10-28 14:56:23 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Serializacao.Models;
+using Newtonsoft.Json;
+
+DateTime dataAtual = DateTime.Now;
+List<Venda> listaVendas = new List<Venda>();
+
+Venda v1 = new Venda(1, "Material de Escritorio", 25.00M, dataAtual);
+Venda v2 = new Venda(2, "Material de Construcao", 350.00M, dataAtual);
+
+listaVendas.Add(v1);
+listaVendas.Add(v2);
+
+// Serializacao
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+File.WriteAllText("Arquivos/vendas.json", serializado);
+
+// Deserializacao
+string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+List<Venda> listaVendas2 = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+
+foreach (Venda venda in listaVendas2) {
+    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, " +
+                $"Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("dd/MM/yy HH:mm")}");
+}
